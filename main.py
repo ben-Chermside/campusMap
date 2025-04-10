266b69844e05fb932cd7bd195eceb6544d3ec1d4
benchermside <bchermside@gmail.com>
2025-04-10 15:59:17 -0400

    adding

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 import tkinter as tk
+import tkinter as tk
+from tkinter import ttk
+
+def callback():
+    message = tk.Label(root, text="Hello, World!")
+    message.pack()
+    
 
 root = tk.Tk()
+
+tk.Label(root, text='Classic Label').pack()
+ttk.Label(root, text='Themed Label').pack()
+button = ttk.Button(
+   root, 
+   text="button3",
+   command=callback
+)
+button.pack(
+    ipadx=5,
+    ipady=5,
+)
+
+message = tk.Label(root, text="Hello, World!")
+# message.pack()
+# root.title('world map')
+
+
+
 root.mainloop()
